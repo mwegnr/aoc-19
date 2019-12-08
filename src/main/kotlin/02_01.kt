28344cd1f122e39f02244507d734b19c41fae2f6
Maik <maikwegener@mailbox.org>
2019-12-08 01:16:08 +0100

    day 2: fixed dumb bug in submission

--- src/main/kotlin/02_01.kt
+++ src/main/kotlin/02_01.kt
@@ -2,34 +2,19 @@ private const val DAY_OF_MONTH = "02"
 
 fun main() {
     val input = getInputLine(DAY_OF_MONTH, 0)
-    print(followInstructionsAndGetResult(getInstructionsAsIntList(input)))
-}
+    val instructionsAsIntList = ArrayList(input.split(",").map { it.toInt() })
+    instructionsAsIntList[1] = 12
+    instructionsAsIntList[2] = 2
 
-fun getInstructionsAsIntList(instructions: String): ArrayList<Int> {
-    val instructionsAsIntList = ArrayList<Int>()
-    instructions.split(",").forEach { it -> instructionsAsIntList.add(it.toInt()) }
-    return instructionsAsIntList
+    print(followInstructionsAndGetResult(instructionsAsIntList)[0])
 }
 
 fun followInstructionsAndGetResult(instructions: ArrayList<Int>): ArrayList<Int> {
-    val firstOperandRegisterOffset = 1
-    val secondOperandRegisterOffset = 2
-    val resultRegisterOffset = 3
-
     for (i in 0 until instructions.size step 4) {
+        val (fstPos, sndPos, resPos) = arrayOf(instructions[i + 1], instructions[i + 2], instructions[i + 3])
         when (instructions[i]) {
-            1 -> {
-                val fstValuePos = instructions[i + firstOperandRegisterOffset]
-                val sndValuePos = instructions[i + secondOperandRegisterOffset]
-                val resultPosition = instructions[i + resultRegisterOffset]
-                instructions[resultPosition] = instructions[fstValuePos] + instructions[sndValuePos]
-            }
-            2 -> {
-                val fstValuePos = instructions[i + 1]
-                val sndValuePos = instructions[i + 2]
-                val resultPosition = instructions[i + 3]
-                instructions[resultPosition] = instructions[fstValuePos] * instructions[sndValuePos]
-            }
+            1 -> instructions[resPos] = instructions[fstPos] + instructions[sndPos]
+            2 -> instructions[resPos] = instructions[fstPos] * instructions[sndPos]
             99 -> return instructions
         }
     }
